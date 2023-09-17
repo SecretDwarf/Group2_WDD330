f59f065b530ae77c5fe069b3a0ecc1f52443bf7e
Dopeycylender39 <briggsjjacob@gmail.com>
2023-09-16 22:33:00 -0600

    commit

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,14 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartContent = getLocalStorage("so-cart");
+  if (cartContent) {
+    cart = cartContent;
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
